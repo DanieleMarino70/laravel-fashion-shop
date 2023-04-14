8ceb0d2186776a839699786352b5f58c5f6cc34c
Alessandro Montenero <alemonte99@icloud.com>
2023-04-14 15:54:25 +0200

    created seeder

--- database/migrations/2023_04_14_133412_create_shoes_table.php
+++ database/migrations/2023_04_14_133412_create_shoes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('shoes', function (Blueprint $table) {
             $table->id();
+            $table->string('manufacturer', 40);
+            $table->string('model', 40);
+            $table->string('material', 100)->nullable();
+            $table->text('description')->nullable();
+            $table->float('price');
+            $table->smallInteger('size');
             $table->timestamps();
         });
     }
