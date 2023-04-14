899ab4bce826355544058f07a2a3a8a851cf7ee4
GretaFormica <greta.formica@gmail.com>
2023-04-14 16:25:32 +0200

    add index

---
+++ resources/views/admin/shoes/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>tabella principale</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/backoffice', function () {
+    return view('admin.shoes.index');
+})->middleware(['auth', 'verified'])->name('backoffice');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
